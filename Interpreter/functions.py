692107de6476a58eb4e9f1ea8a125e3e40566c85
Vincent Emonet <vincent.emonet@gmail.com>
2021-11-24 21:35:33 +0100

    add setup.py to install dragoman as a CLI, add dockerfile.cli to run as CLI using docker, add 2 functions: split_string and process_string, add a complete example in example/hgnc

--- Interpreter/functions.py
+++ Interpreter/functions.py
@@ -13,7 +13,7 @@ global functions_pool
 #####################################################################################################
 
 ## For each new function that you define, add an entry as "function_name":"" to the dictionary below 
-functions_pool = {"tolower":"","chomp":""}
+functions_pool = {"tolower":"","chomp":"","process_string":""}
 
 
 ## Define your functions here following examples below, the column "names" from the csv files 
@@ -25,6 +25,61 @@ def tolower():
 def chomp():
     return global_dic["value"].replace(global_dic["toremove"], '')
 
+
+def split_string():
+    return global_dic["value"].split(global_dic["split"])
+
+def process_string():
+    s = None
+    if "input" in global_dic.keys():
+        s = global_dic["input"]
+    split = None
+    if "split_on" in global_dic.keys():
+        split = global_dic["split_on"]
+    prefix = None
+    if "add_prefix" in global_dic.keys():
+        prefix = global_dic["add_prefix"]
+    find = None
+    if "find" in global_dic.keys():
+        find = global_dic["find"]
+    replace = None
+    if "replace" in global_dic.keys():
+        replace = global_dic["replace"]
+    format = None
+    if "format_for" in global_dic.keys():
+        format = global_dic["format_for"]
+
+    if not s:
+        return None
+    
+    resultList = []
+    if split:
+        resultList = s.split(split)
+    else:
+        resultList.append(s)
+    
+    if prefix or replace or format:
+        for i in resultList:
+            if format and format.lower() == "uri":
+                resultList[i] = resultList[i].replace(" ", "-").replace(",", "-").replace(".", "-").lower();
+            if format and format.lower() == "lowercase":
+                resultList[i] = resultList[i].toLowerCase()
+
+            if format and format.lower() == "uppercase":
+                resultList[i] = resultList[i].upper()
+
+            if prefix:
+                resultList[i] = prefix + resultList[i];
+            
+            if find and replace:
+                # resultList[i] = resultList[i].replaceAll(find, replace);
+                regex = re.compile(find)
+                resultList[i] = re.sub(regex, replace, resultList[i])
+
+    return resultList
+
+
+    # return global_dic["value"].replace(global_dic["toremove"], '')
 
 
 ################################################################################################
